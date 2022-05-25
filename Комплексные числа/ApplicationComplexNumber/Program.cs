f2291d8e41e4946048e4f5d3da922a7f9f7a8637
SuhanovaE <98471539+SuhanovaE@users.noreply.github.com>
2022-05-25 10:22:58 +0300

    Add files via upload

---
+++ Комплексные числа/ApplicationComplexNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibraryComplexNumber;
+
+namespace ApplicationComplexNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Первое число");
+            ClassComplexNumber complexNumber = new ClassComplexNumber();
+            complexNumber.X = 112.2;
+            complexNumber.Y = 9.56;
+            Console.WriteLine(complexNumber.ToString());
+            Console.WriteLine("Второе число");
+            ClassComplexNumber complexNumber1 = new ClassComplexNumber();
+            complexNumber1.X = double.Parse(Console.ReadLine());
+            complexNumber1.Y = double.Parse(Console.ReadLine());
+            Console.WriteLine(complexNumber1.ToString());
+            
+
+            Console.WriteLine("Сумма:");                     
+            Console.WriteLine(complexNumber.Summation(complexNumber1).ToString());
+
+            Console.WriteLine("Разность:");
+            Console.WriteLine(complexNumber.Substraction(complexNumber1).ToString());
+
+            Console.WriteLine("Умножени:");
+            Console.WriteLine(complexNumber.Multiplication(complexNumber1).ToString());
+
+            Console.WriteLine("Деление:");
+            Console.WriteLine(complexNumber.Divisionm(complexNumber1).ToString());
+
+            ClassComplexNumber complexNumber3 = new ClassComplexNumber(44, -100);
+            Console.WriteLine(complexNumber3.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Комплексные числа/ClassLibraryComplexNumber/ClassComplexNumber.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryComplexNumber
+{ 
+    /// <summary>
+    /// библиотека класса комплексного числа
+    /// </summary>
+    public class ClassComplexNumber
+    {
+        //x + y*i;
+        private double x;//действительная
+        private double y;//мнимая
+
+        public double X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        public double Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        /// <summary>
+        /// конструкторы
+        /// </summary>
+        public ClassComplexNumber() //конструктор по умолчанию
+        {
+            x = 0;
+            y = 0;
+        }
+        public ClassComplexNumber(double a, double b) // конструктор с параметрами
+        {
+            x = a;
+            y = b;
+        }
+        public override string ToString()// перезагрухка метода ToString()
+        {
+            return $"{x} + {y}*i";
+        }
+        public string Summation(ClassComplexNumber complex2)//сумма
+        {
+            ClassComplexNumber c3 = new ClassComplexNumber();
+            c3.x = x + complex2.x;
+            c3.y = y + complex2.y;
+            return c3.ToString();
+        }
+        public string Substraction(ClassComplexNumber complex2)//вычитание
+        {
+            ClassComplexNumber c3 = new ClassComplexNumber();
+            c3.x = x - complex2.x;
+            c3.y = y - complex2.y;
+            return c3.ToString();
+        }
+        //добавить умножение
+        public string Multiplication(ClassComplexNumber complex2)//вычитание
+        {
+            ClassComplexNumber c3 = new ClassComplexNumber();
+            c3.x = (x * complex2.x - y * complex2.y);
+            c3.y = (x * complex2.y - y * complex2.x);
+            return c3.ToString();
+        }
+        //деление
+        public string  Divisionm(ClassComplexNumber complex2)//вычитание
+        {
+            ClassComplexNumber c3 = new ClassComplexNumber();
+            c3.x = (x * complex2.x + y * complex2.y)/(complex2.x* complex2.x + complex2.y* complex2.y);
+            c3.y = (y * complex2.x - x * complex2.y) / (complex2.x * complex2.x + complex2.y * complex2.y);
+            return c3.ToString();
+        }
+
+    }
+}
